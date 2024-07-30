2baeba5b9293f608e9341b93c5e8dddb3d016f18
Taisuke Maekawa <fygar256@gmail.com>
2024-07-30 23:53:34 +0900

    Add files via upload

--- prime6.py
+++ prime6.py
@@ -1,1 +1,20 @@
+#!/usr/bin/python3
+import sys
+prime=[2]
+def sub1(n):
+    for p in prime:
+        if n**0.5<p:
+            return True
+        if n%p==0:
+            return False
 
+def main():
+    global prime
+    n=int(sys.argv[1])
+    for i in range(3,n+1):
+        if sub1(i):
+            prime+=[i]
+    print(f"{len(prime)} primes{prime}")
+
+if __name__=="__main__":
+    main()
