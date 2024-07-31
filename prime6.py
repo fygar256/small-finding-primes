26deaec57c83a634d7257f89c9b4300582c20316
Taisuke Maekawa <fygar256@gmail.com>
2024-07-31 22:36:35 +0900

    Add files via upload

--- prime6.py
+++ prime6.py
@@ -1,8 +1,7 @@
-m=[2]
 def s(n):
-    for p in m:
-        if n**0.5<p: return 1
-        if not n%p: return 0
-for i in range(3,int(input("N:"))+1):
-    if s(i): m+=[i]
-print(len(m),m)
+ for p in m:
+  if n%p==0:return 0 
+ return 1
+m=[]
+for i in range(2,100):
+ if s(i):m+=[i];print(i)
